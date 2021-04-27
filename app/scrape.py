87d8f873d0f0f6f6f08d5eaf959f2b3b38a76137
fjohngaughan <fjohngaughan@gmail.com>
2021-04-27 16:55:00 -0400

    from env import GOOGLECHROM_CHROME_BIN etc

--- app/scrape.py
+++ app/scrape.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from env import GOOGLE_CHROME_BIN, CHROMEDRIVER_PATH
 import pandas as pd 
 import numpy as np
 from datetime import date
