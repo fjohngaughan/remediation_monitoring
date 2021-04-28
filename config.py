7749845d71bb3eba6bd9b40169aac7ceb4fa355e
fjohngaughan <fjohngaughan@gmail.com>
2021-04-28 01:07:25 -0400

    still trying to figure out database

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess...'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://igeqpwwqvxgurd:a12322967b1b7f9ff3a69e767a73fba1a5f04487b9743097eebf91c202b78740@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d14ap4jj8k91hr' or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
