c650e6d4579e5c403643e4116c40a20b954c032f
fjohngaughan <fjohngaughan@gmail.com>
2021-05-13 22:13:36 -0400

    update sqlalchemy_database_uri

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess...'
-    SQLALCHEMY_DATABASE_URI = 'postgres://igeqpwwqvxgurd:a12322967b1b7f9ff3a69e767a73fba1a5f04487b9743097eebf91c202b78740@ec2-3-217-219-146.compute-1.amazonaws.com:5432/d14ap4jj8k91hr' or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://eebpdvnqyhkrzf:facee458508103e87641807380e78c786f1043780f766caa473cdc06ea8ee27e@ec2-34-206-8-52.compute-1.amazonaws.com:5432/d2j5rn153cv6fv' or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
