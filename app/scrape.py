1c30cf48ec5ef607e6683482badad32958012f51
John Gaughan <75457317+john-gaughan@users.noreply.github.com>
2021-06-01 20:48:01 -0700

    Update scrape.py

--- app/scrape.py
+++ app/scrape.py
@@ -11,17 +11,19 @@ from datetime import date
 from time import sleep
 from sqlalchemy import create_engine
 
+# Web Scraping Setup
 today = date.today()
-
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
-
 gt_start_url = 'https://geotracker.waterboards.ca.gov/profile_report?global_id='
 driver = webdriver.Chrome(options=options, executable_path='/Users/fjgaughan94/Desktop/data-science/coding-temple/final-project/ideas-&-testing/selenium-test/chrome_driver/chromedriver')
 database_uri = 'postgres://cglpswlj:PljB6Lqcs31c_WxbjZ8w77WNxJVty0zU@queenie.db.elephantsql.com:5432/cglpswlj'
 engine = create_engine(database_uri)
 
+# This class is instantiated and run when a user saves or updates a report--it locates, then saves to our database, the newest action for each site under the 
+# "Regulatory Activities" tab on GeoTracker. This allows the program to determine if any updates have been posted when the user clicks the "scan for updates" button 
+# for the first time.
 class initial_site_scan():
 
     def __init__(self, sites_list, report_id):
@@ -36,11 +38,12 @@ class initial_site_scan():
         
     def scrape_site_update(self, report_update):
         for site in self.sites_list:
-            # Navigating to the cleanup site page
+            # Navigate to the site's page on GeoTracker using its Global ID
             driver.get(gt_start_url + site[1])
-            # Grabbing the site's status
+            # Get and clean the site's status
             site_status = driver.find_element_by_xpath('//*[@id="main-content"]/div/main/div/div[2]/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr[5]/td/font').text
             site_status = site_status.rstrip(" - DEFINITION")
+            # Save the status to our database
             new_site_update = SiteUpdate(report_update_id=report_update.id, site_id=site[0], site_status=site_status)
             db.session.add(new_site_update)
             db.session.commit()
@@ -48,17 +51,18 @@ class initial_site_scan():
         return
 
     def scrape_top_action(self, site_update):
-        # Navigating to the cleanup site page
+        # Navigate to the site's page on GeoTracker using its Global ID (pulled from our database).
         driver.get(gt_start_url + site_update.site.gt_global_id)
-        # Navigate to the Regulatory Activiaties Tag
+        # Navigate to the Regulatory Activities Tag
         try:
             driver.find_element_by_link_text("Regulatory Activities").click()
-            # Wait for the page to load
+            # Give the page time to load
             xpath = "//table[@id='mytab']/tbody/tr/td/table/tbody/tr[2]/td[4]"
             try:
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
             except TimeoutException:
                 print("Required element still could not be found!")
+            # Get the newest action under the Regulatory Activities Tab, and save to our database
             action_date = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[2]/td[4]").text
             description = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[2]/td[6]").text
             action_type = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[2]/td[2]").text
@@ -72,6 +76,8 @@ class initial_site_scan():
         except:
             return
 
+# This class is instantiated and run when a user hits the "scan for updates" button on a report--it locates, then saves to our database, any new actions for each site under the 
+# "Regulatory Activities" tab on GeoTracker. 
 class report_update_scan():
 
     def __init__(self, report_id):
@@ -80,11 +86,9 @@ class report_update_scan():
         # Get the last site update for each site that's saved in our database
         last_site_updates = [site.site_updates[-1] for site in report.sites]
         # Create a list of lists with the site id, GeoTracker global id, and site update id for each site, drawn from last_site_updates
-        sites_list = [[site_update.site_id, site_update.site.gt_global_id, site_update.id] for site_update in last_site_updates]
+        self.sites_list = [[site_update.site_id, site_update.site.gt_global_id, site_update.id] for site_update in last_site_updates]
         self.last_site_updates_ids = [site_update.id for site_update in last_site_updates]
-        self.sites_list = sites_list
         self.report_id = report_id
-
 
     def start(self):
         new_report_update = ReportUpdate(report_id=self.report_id)
@@ -92,15 +96,15 @@ class report_update_scan():
         db.session.commit()
         self.scrape_site_update(new_report_update)
 
-
     def scrape_site_update(self, report_update):
         for site in self.sites_list:
-            # Navigating to the cleanup site page
+            # Navigate to the site's page on GeoTracker using its Global ID
             driver.get(gt_start_url + site[1])
-            # Grabbing the site's status
+            # Compare the site's current status with the last status saved in our database
             prev_site_status = Site.query.filter_by(id=site[0]).first().site_updates[-1].site_status
             site_status = driver.find_element_by_xpath('//*[@id="main-content"]/div/main/div/div[2]/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr[5]/td/font').text
             site_status = site_status.rstrip(" - DEFINITION")
+            # Save the new site update to our database
             if site_status != prev_site_status:
                 new_site_update = SiteUpdate(report_update_id=report_update.id, site_id=site[0], site_status=site_status, status_changed=True)
                 db.session.add(new_site_update)
@@ -114,32 +118,30 @@ class report_update_scan():
 
 
     def scrape_new_actions(self, site_update):
-        # Navigating to the cleanup site page
+        # Navigate to the site's page on GeoTracker using its Global ID
         driver.get(gt_start_url + site_update.site.gt_global_id)
         # Check if the Regulatory Activities tab is active
         if driver.find_element_by_xpath("//a[@class='tab-disabled']/span").text == 'Regulatory Activities':
             return 
         # Navigate to the Regulatory Activities tab
         driver.find_element_by_link_text("Regulatory Activities").click()
-        # Wait for the page to load
+        # Give the page time to load
         xpath = "//table[@id='mytab']/tbody/tr/td/table/tbody/tr[2]/td[4]"
         try:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
         except TimeoutException:
             print("Required element still could not be found!")
-        # loop through Regulatory Activity rows until it hits a row in the "New Actions" table
+        # Loop through Regulatory Activity rows until it hits a row that is in the "New Actions" table in our database
         last_site_update_w_action = SiteUpdate.query.filter_by(site_id=site_update.site_id).filter(SiteUpdate.new_actions.any()).all()[-1]
         last_site_action = last_site_update_w_action.new_actions[0]
         for i in range(2,10):
-            # Check if action date and description in the row are in the "New Actions" table
+            # Check if action date and description in the row are in the "New Actions" table in our database
             action_date = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{i}]/td[4]").text
             description = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{i}]/td[6]").text
-            # I'm checking if the top action is in the last Site Update
-            # The last site update will have the appropriate date to check against.
-            # I need to make a list of the action dates from the last Site Update and see if action_date is in there
             if action_date == last_site_action.action_date and description == last_site_action.description:
                 break
             else:
+                # If the action is not in our database, get the row's data and save it to our database
                 action_type = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{i}]/td[2]").text
                 action = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{i}]/td[3]").text
                 received_date = driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{i}]/td[5]").text
@@ -151,24 +153,19 @@ class report_update_scan():
 
 
     def scrape_new_docs(self, site_update):
-        # Navigate to the cleanup site page
+        # Navigate to the site's page on GeoTracker using its Global ID
         driver.get(gt_start_url + site_update.site.gt_global_id)
-        # Navigate to the Regulatory Activiaties Tag
+        # Navigate to the Regulatory Activities Tab
         driver.find_element_by_link_text("Regulatory Activities").click()
-        # Wait for the page to load
         # Loop through each of the new actions just appended to the site update
         for i in range(2, (len(site_update.new_actions)+2)):
-            # Click on "[VIEW DOCS]" for the current new_action
-            # How do we get the table row number? 
-            # if driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{int(i+2)}]/td[1]/a") is False:
-            #     continue
-            # else: 
-            # THE XPATH ABSOLUTELY EXISTS. SO THIS SHOULD BE A PROBLEM WITH THE WINDOW VIEW CHANGE. 
+            # Give the page time to load
             xpath = "//table[@id='mytab']/tbody/tr/td/table/tbody/tr[2]/td[4]"
             try:
                 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
             except TimeoutException:
                 print("Required element still could not be found!") 
+            # Click on "[VIEW DOCS]" for the current new_action
             if driver.find_element_by_xpath(f"//table[@id='mytab']/tbody/tr/td/table/tbody/tr[{i}]/td[1]").text != "[VIEW DOCS]":
                 continue
             else:
@@ -177,12 +174,12 @@ class report_update_scan():
                 site_page = driver.window_handles[0]
                 view_docs_page = driver.window_handles[1]
                 driver.switch_to.window(view_docs_page)
-                # Wait for the page to load
+                # Give the page time to load
                 try:
                     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@href]")))
                 except TimeoutException:
                     print("Required element still could not be found!")
-                # Get each doc's pdf link and name
+                # Get each doc's pdf link and name, and save to our database
                 pdf_links = driver.find_elements_by_xpath("//a[@href]")
                 for pdf in pdf_links:
                     link = pdf.get_attribute('href')
